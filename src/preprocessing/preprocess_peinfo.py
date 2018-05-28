2e825c82365a81c1d91d5644dd8b058721c9a28d
Chun-Te Sung <chunte.sung@tum.de>
2018-05-28 16:27:09 +0200

    Supported saving data to Cassandra

--- src/preprocessing/preprocess_peinfo.py
+++ src/preprocessing/preprocess_peinfo.py
@@ -4,7 +4,7 @@ peinfo_rdd = sqlContext.read.parquet(DF_LOCATION).rdd
 peinfo_results = peinfo_rdd.map(lambda x: (x.sha256, find_val_in_peinfo(x.results)))
 
 def find_val_in_peinfo(results):
-    val_list = [0] * 16
+    val_list = [0.0] * 16
 
     peinfo = json.loads(results)
 
@@ -21,10 +21,14 @@ def find_val_in_peinfo(results):
             continue
 
         val_list[i * 4] = s["entrophy"]
-        val_list[i * 4 + 1] = s["VirtualAddress"]["Value"]
-        val_list[i * 4 + 2] = s["Misc_VirtualSize"]["Value"]
-        val_list[i * 4 + 3] = s["SizeOfRawData"]["Value"]
+        val_list[i * 4 + 1] = s["VirtualAddress"]["Value"] + 0.0
+        val_list[i * 4 + 2] = s["Misc_VirtualSize"]["Value"] + 0.0
+        val_list[i * 4 + 3] = s["SizeOfRawData"]["Value"] + 0.0
 
-    val_list.append(int(peinfo["HEADERS"]["FILE_HEADER"]["TimeDateStamp"]["Value"].split(" ")[0], 16))
+    val_list.append(int(peinfo["HEADERS"]["FILE_HEADER"]["TimeDateStamp"]["Value"].split(" ")[0], 16) + 0.0)
 
     return val_list
+
+peinfo_df = peinfo_results.toDF()
+peinfo_df = peinfo_df.withColumnRenamed("_1", "sha256").withColumnRenamed("_2", "features")
+peinfo_df.write.format("org.apache.spark.sql.cassandra").mode('append').options(table=PEINFO_RESULTS_TABLE, keyspace=KEYSPACE).save()

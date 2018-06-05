b7956ff3ecaacfbc01983cea316f0a98b5f04469
Chun-Te Sung <chunte.sung@tum.de>
2018-06-05 09:17:36 +0200

    Changed to save a list of protobuf objects

--- src/feedhandling/feed_client.py
+++ src/feedhandling/feed_client.py
@@ -4,37 +4,28 @@ import pickle
 import feed_handling_pb2
 import feed_handling_pb2_grpc
 
-def save_to_file(service_name, feature_list, label_list):
-    f = open(service_name + '_features.p', 'wb')
-    l = open(service_name + '_labels.p', 'wb')
+def save_to_file(service_name, fl_list):
+    f = open(service_name + '_features_and_labels.p', 'wb')
 
-    pickle.dump(feature_list, f)
-    pickle.dump(label_list, l)
+    pickle.dump(fl_list, f)
 
     f.close()
-    l.close()
 
 def get_training_data(stub):
     rows = stub.GetTrainingData(feed_handling_pb2.Empty())
 
-    cuckoo_feature_list = []
-    objdump_feature_list = []
-    peinfo_feature_list = []
-    richheader_feature_list = []
-
-    cuckoo_label_list = []
-    objdump_label_list = []
-    peinfo_label_list = []
-    richheader_label_list = []
+    cuckoo_fl_list = []
+    objdump_fl_list = []
+    peinfo_fl_list = []
+    richheader_fl_list = []
 
     for r in rows:
-        locals()[r.service_name.lower() + '_feature_list'].append(r.features)
-        locals()[r.service_name.lower() + '_label_list'].append(r.labels)
+        locals()[r.service_name.lower() + '_fl_list'].append(r.SerializeToString())
 
-    save_to_file('cuckoo', cuckoo_feature_list, cuckoo_label_list)
-    save_to_file('objdump', objdump_feature_list, objdump_label_list)
-    save_to_file('peinfo', peinfo_feature_list, peinfo_label_list)
-    save_to_file('richheader', richheader_feature_list, richheader_label_list)
+    save_to_file('cuckoo', cuckoo_fl_list)
+    save_to_file('objdump', objdump_fl_list)
+    save_to_file('peinfo', peinfo_fl_list)
+    save_to_file('richheader', richheader_fl_list)
 
 def run():
     channel = grpc.insecure_channel(SERVER_IP + ':50051')
@@ -43,4 +34,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-

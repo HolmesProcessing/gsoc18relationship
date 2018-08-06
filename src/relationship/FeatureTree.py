d2753f6bd15fe2608bc85e279e66973d8d9eaa70
Chun-Te Sung <chunte.sung@tum.de>
2018-08-06 19:17:26 +0200

    Updated for relationship scores based on multi-label classifications

--- src/relationship/FeatureTree.py
+++ src/relationship/FeatureTree.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import tensorflow as tf
 import feedhandling.feed_handling_pb2 as feed_handling_pb2
+import time
 
 from sklearn import preprocessing
 from sklearn.neighbors import KDTree
@@ -17,21 +18,49 @@ from collections import Counter
 tf.app.flags.DEFINE_string('server', 'localhost:9000', 'PredictionService host:port')
 FLAGS = tf.app.flags.FLAGS
 
-def encode_label(label):
-    labels = ['CryptoRansom', 'apt1', 'athena_variant', 'betabot', 'blackshades', 'citadel_krebs', 'darkcomet', 'darkddoser', 'dirtjumper', 'expiro', 'gamarue', 'ghostheart2', 'locker', 'machbot', 'mediyes', 'nitol', 'pushdo', 'shylock', 'simda', 'yoyoddos2']
-    return labels.index(label)
+def encode_label(labels):
+    label = ['CryptoRansom', 'apt1', 'athena_variant', 'betabot', 'blackshades', 'citadel_krebs', 'darkcomet', 'darkddoser', 'dirtjumper', 'expiro', 'gamarue', 'ghostheart2', 'locker', 'machbot', 'mediyes', 'nitol', 'pushdo', 'shylock', 'simda', 'yoyoddos2']
+
+    switcher = {
+        'athena_variant': 20,   # CIA Malware
+        'betabot': 21,          # Hijacker
+        'blackshades': 22,      # Trojan
+        'darkcomet': 22,
+        'nitol': 22,
+        'shylock': 22,
+        'citadel_krebs':23,     # Zeus offshoot
+        'darkdoser':24,         # Password stealing tool
+        'dirtjumper':25,        # DDoS Bot
+        'expiro': 26,           # Virus
+        'gamarue': 27,          # Worm
+        'machbot': 28,          # Botnet
+        'pushdo': 28,
+        'simda': 28,
+        'yoyoddos2': 28
+    }
+
+    classifications = []
+
+    for l in labels:
+        classifications.append(label.index(l))
+
+    if len(classifications) == 1 and switcher.get(label[classifications[0]]):
+        classifications.append(switcher.get(label[classifications[0]]))
+
+    return classifications
 
 class FeatureTree:
     def __init__(self, file_path):
         self.file_path = file_path
 
     def prepare_data(self):
-        self.X_cuckoo = []
-        self.X_objdump = []
-        self.X_peinfo = []
-        self.X_richheader = []
         self.sha256 = []
-        self.label = []
+        self.labels = []
+
+        X_cuckoo = []
+        X_objdump = []
+        X_peinfo = []
+        X_richheader = []
 
         objects = pickle.load(open(self.file_path, 'rb'))
 
@@ -39,37 +68,43 @@ class FeatureTree:
         for o in objects:
             reader.ParseFromString(o)
 
+            try:
+                self.labels.append(encode_label(reader.labels))
+            except:
+                continue
+
             if not reader.features_cuckoo:
-                self.X_cuckoo.append(np.zeros(150))
+                X_cuckoo.append(np.zeros(150))
             else:
-                self.X_cuckoo.append(np.array(map(int, reader.features_cuckoo)))
+                X_cuckoo.append(np.array(map(int, reader.features_cuckoo)))
 
             if not reader.features_objdump:
-                self.X_objdump.append(np.zeros(100))
+                X_objdump.append(np.zeros(100))
             else:
-                self.X_objdump.append(np.array(map(float, reader.features_objdump)))
+                X_objdump.append(np.array(map(float, reader.features_objdump)))
 
             if not reader.features_peinfo:
-                self.X_peinfo.append(np.zeros(17))
+                X_peinfo.append(np.zeros(17))
             else:
-                self.X_peinfo.append(np.array(map(float, reader.features_peinfo)))
+                X_peinfo.append(np.array(map(float, reader.features_peinfo)))
 
             if not reader.features_richheader:
-                self.X_richheader.append(np.zeros(80))
+                X_richheader.append(np.zeros(80))
             else:
-                self.X_richheader.append(np.array(map(float, reader.features_richheader)))
+                X_richheader.append(np.array(map(float, reader.features_richheader)))
 
             self.sha256.append(reader.sha256)
-            self.label.append(encode_label(reader.label))
 
-        self.cnn_features = np.array(self.X_cuckoo).astype(np.int32)
+        self.cnn_features = np.array(X_cuckoo).astype(np.int32)
 
-        self.X_objdump = preprocessing.MinMaxScaler().fit_transform(self.X_objdump)
-        self.X_peinfo = preprocessing.MinMaxScaler().fit_transform(self.X_peinfo)
-        self.X_richheader = preprocessing.MinMaxScaler().fit_transform(self.X_richheader)
-        self.mlp_features = np.concatenate((self.X_objdump, self.X_peinfo, self.X_richheader), axis=1).astype(np.float32)
+        X_objdump = preprocessing.MinMaxScaler().fit_transform(X_objdump)
+        X_peinfo = preprocessing.MinMaxScaler().fit_transform(X_peinfo)
+        X_richheader = preprocessing.MinMaxScaler().fit_transform(X_richheader)
+        self.mlp_features = np.concatenate((X_objdump, X_peinfo, X_richheader), axis=1).astype(np.float32)
 
-        self.hidden_features = np.empty([1, 257])
+        self.labels = np.array(self.labels)
+        self.hidden_features = np.empty([1, 29])
+        self.predicted_labels = np.empty([1, 29])
 
     def get_hidden_features(self, hostport, chunk_size=1000):
         print('Start retrieving hidden features ...\n')
@@ -95,31 +130,45 @@ class FeatureTree:
 
             response = stub.Predict(request, 5.0)
 
-            self.hidden_features = np.concatenate((self.hidden_features, tensor_util.MakeNdarray(response.outputs['h_concat'])), axis=0)
+            self.hidden_features = np.concatenate((self.hidden_features, tensor_util.MakeNdarray(response.outputs['y_raw'])), axis=0)
+            self.predicted_labels = np.concatenate((self.predicted_labels, tensor_util.MakeNdarray(response.outputs['label'])), axis=0)
 
             chunk_id += 1
 
-        self.hidden_features = np.delete(self.hidden_features, 1, 0)
+        self.hidden_features = np.delete(self.hidden_features, 0, 0)
+        self.predicted_labels = np.delete(self.predicted_labels, 0, 0)
+        self.predicted_labels = [self.decode_one_hot(l) for l in self.predicted_labels]
         print('Received all features!\n')
+
+    def decode_one_hot(self, labels):
+        decoded_labels = []
+        for i in range(len(labels)):
+            if labels[i] == 1:
+                decoded_labels.append(i)
+
+        return decoded_labels
 
     def build_and_save_feature_tree(self):
         print('Start building and saving the feature tree ...\n')
-        self.tree = KDTree(self.hidden_features, leaf_size=257)
+        self.tree = KDTree(self.hidden_features, leaf_size=29)
 
         tree_f = open('ftree.p', 'wb')
         sha256_f = open('sha256.p', 'wb')
         hf_f = open('hf.p', 'wb')
-        label_f = open('label.p', 'wb')
+        labels_f = open('labels.p', 'wb')
+        predicted_labels_f = open('predicted_labels.p', 'wb')
 
         pickle.dump(self.tree, tree_f)
         pickle.dump(self.sha256, sha256_f)
         pickle.dump(self.hidden_features, hf_f)
-        pickle.dump(self.label, label_f)
+        pickle.dump(self.labels, labels_f)
+        pickle.dump(self.predicted_labels, predicted_labels_f)
 
         tree_f.close()
         sha256_f.close()
         hf_f.close()
-        label_f.close()
+        labels_f.close()
+        predicted_labels_f.close()
 
         print('Everything is done and goes well! :)\n')
 
@@ -127,21 +176,18 @@ class FeatureTree:
         self.tree = pickle.load(open('ftree.p', 'rb'))
         self.sha256 = pickle.load(open('sha256.p', 'rb'))
         self.hidden_features = pickle.load(open('hf.p', 'rb'))
-        self.label = pickle.load(open('label.p', 'rb'))
+        self.labels = pickle.load(open('labels.p', 'rb'))
 
-        freqs = []
+        for j in range(len(self.labels)):
+            dist, ind = self.tree.query(self.hidden_features[j,:].reshape(1, -1), k=100)
 
-        for j in range(len(self.label)):
-            dist, ind = self.tree.query(self.hidden_features[j,:].reshape(1, -1), k=10)
+            matched = []
+            for i in range(len(ind[0])):
+                if set(self.labels[j]) == set(self.labels[ind[0][i]]):
+                    matched.append(i)
 
-            labels = []
-            for i in ind[0]:
-                labels.append(self.label[i])
-
-            freq = Counter(labels)
-            freqs.append(freq.most_common(1)[0][1])
-
-        print(Counter(freqs))
+            print(self.labels[j])
+            print(matched)
 
 def main(_):
     ft = FeatureTree('./objects.p')
--- src/tfl_server.py
+++ src/tfl_server.py
@@ -1,26 +1,62 @@
 from concurrent import futures
+
 import time
 import grpc
 import argparse
+import pickle
+import tensorflow as tf
 
 from feedhandling import feed_handling_pb2
 from feedhandling import feed_handling_pb2_grpc
 from tflearning import tf_learning_pb2
 from tflearning import tf_learning_pb2_grpc
 from tflearning.NN import NN
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+from tensorflow.python.framework import tensor_util
 import get_training_data
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+tf.app.flags.DEFINE_string('server', 'localhost:9000', 'PredictionService host:port')
+FLAGS = tf.app.flags.FLAGS
 
 def train_model():
     nn_instance = NN("./objects.p", label_length=20)
-    kf = nn_instance.split_train_test(3, 0)
+    skf = nn_instance.split_train_test(3, 0)
 
-    for train_index, test_index in kf:
+    for train_index, test_index in skf:
         nn_instance.prepare_data(train_index, test_index)
         nn_instance.train()
 
     return nn_instance
+
+def convert_to_name(labels):
+    label = ['CryptoRansom', 'apt1', 'athena_variant', 'betabot', 'blackshades', 'citadel_krebs', 'darkcomet', 'darkddoser', 'dirtjumper', 'expiro', 'gamarue', 'ghostheart2', 'locker', 'machbot', 'mediyes', 'nitol', 'pushdo', 'shylock', 'simda', 'yoyoddos2', 'CIA Malware', 'Hijacker', 'Trojan', 'Zeus offshoot', 'Password stealing tool', 'DDoS Bot', 'Virus', 'Worm', 'Botnet']
+
+    names = []
+
+    for i in labels:
+        names.append(label[i])
+
+    return names
+
+def get_hidden_features(self, hostport, mlp_features, cnn_features):
+    host, port = hostport.split(':')
+    channel = implementations.insecure_channel(host, int(port))
+    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'malware'
+    request.model_spec.signature_name = tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
+
+    request.inputs['mlp_features'].CopyFrom(tf.contrib.util.make_tensor_proto(mlp_features, shape=mlp_features.shape))
+    request.inputs['cnn_features'].CopyFrom(tf.contrib.util.make_tensor_proto(cnn_features, shape=cnn_features.shape))
+    request.inputs['keep_prob'].CopyFrom(tf.contrib.util.make_tensor_proto(1.0))
+
+    response = stub.Predict(request, 5.0)
+
+    return tensor_util.MakeNdarray(response.outputs['label'])
+
 
 class TFLearningServicer(tf_learning_pb2_grpc.TFLearningServicer):
     def __init__(self, verbose):
@@ -29,10 +65,48 @@ class TFLearningServicer(tf_learning_pb2_grpc.TFLearningServicer):
         # start tensorflow_model_server
 
     def PredictLabel(self, request, context):
-        pass
+        if self.verbose:
+            print('[Request] PredictLabel()')
+            print('[Info] Preparing malware features to predict the label')
+
+        X_objdump = preprocessing.MinMaxScaler().fit_transform(request.features_cuckoo)
+        X_peinfo = preprocessing.MinMaxScaler().fit_transform(request.features_objdump)
+        X_richheader = preprocessing.MinMaxScaler().fit_transform(request.features_peinfo)
+        mlp_features = np.concatenate((X_objdump, X_peinfo, X_richheader), axis=1).astype(np.float32)
+        cnn_features = np.array(request.features_richheader).astype(np.int32)
+
+        if self.verbose:
+            print('[Info] Predicting the label')
+
+        labels = get_hidden_features(FLAGS.server, mlp_features, cnn_features)
+
+        for i in labels:
+            yield tf_learning_pb2.Labels(labels=labels)
+
+        if self.verbose:
+            print('[Info] Predicted label sent!')
 
     def GetRelationships(self, request, context):
-        pass
+        if self.verbose:
+            print('[Request] GetRelationships()')
+            print('[Info] Preparing malware relationships')
+
+        tree = pickle.load(open('ftree.p', 'rb'))
+        sha256 = pickle.load(open('sha256.p', 'rb'))
+        hidden_features = pickle.load(open('hf.p', 'rb'))
+        labels = pickle.load(open('labels.p', 'rb'))
+
+        if request.sha256 in sha256:
+            j = sha256.index(request.sha256)
+            dist, ind = tree.query(hidden_features[j,:].reshape(1, -1), k=2000)
+
+            yield tf_learning_pb2.Relationships(sha256=sha256[j], labels=convert_to_name(labels[j]), distance=0)
+            for i in range(len(ind[0])):
+                if j != ind[0][i] and set(labels[j]) == set(labels[ind[0][i]]):
+                    yield tf_learning_pb2.Relationships(sha256=sha256[ind[0][i]], labels=convert_to_name(labels[ind[0][i]]), distance=dist[0][i])
+
+            if self.verbose:
+                print('[Info] Relationship sent!')
 
     def TrainModel(self, request, context):
         if self.verbose:
